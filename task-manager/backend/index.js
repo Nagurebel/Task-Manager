d376f448c6eb8390509f3533ae4a99ef4d692172
Nagarajki <nagarajki@ganakalabs.com>
2025-04-09 23:04:11 +0530

    vercel file added

--- task-manager/backend/index.js
+++ task-manager/backend/index.js
@@ -45,4 +45,6 @@ mongoose.connect(process.env.MONGODB_URI)
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-}); 
+});
+
+module.exports = app;
